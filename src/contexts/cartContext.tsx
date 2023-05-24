580173e7391d81e51477822aaec9b1d846f4fe40
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-05-24 19:14:29 -0300

    add cartContext

---
+++ src/contexts/cartContext.tsx
@@ -0,0 +1,64 @@
+import { createContext, useReducer } from "react";
+import { Product } from "../types";
+
+export type CartProduct = Omit<Product, "inventary"> & {
+  model: string;
+  size: string;
+  quantity: number;
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type Action = {
+  type: "add" | "change" | "delete";
+  product: CartProduct;
+  number: number;
+};
+
+type Context = {
+  cartList: CartProduct[];
+  reduceCartList: React.Dispatch<Action>;
+};
+
+const CartContext = createContext<Context | null>(null);
+
+const CartProvider = ({ children }: Props) => {
+  const cartReducer = (
+    state: CartProduct[],
+    { type, number, product }: Action
+  ): CartProduct[] => {
+    const index = cartList.findIndex(
+      ({ _id, size, model }) =>
+        _id == product._id && size == product.size && model == product.model
+    );
+    const newCart = structuredClone(cartList);
+    if (type == "add") {
+      if (index >= 0) {
+        newCart[index].quantity += number;
+        return newCart;
+      }
+      return [...newCart, product];
+    }
+    if (type == "change") {
+      newCart[index].quantity = number;
+      return newCart;
+    }
+    if (type == "delete") {
+      delete newCart[index];
+      return newCart.filter((e) => e != null);
+    }
+    return state;
+  };
+
+  const [cartList, reduceCartList] = useReducer(cartReducer, []);
+
+  return (
+    <CartContext.Provider value={{ cartList, reduceCartList }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
