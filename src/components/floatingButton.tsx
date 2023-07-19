1760cae67c85fcda35780d76e34cdb0e71ab58b4
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-07-19 06:31:18 -0300

    change phone number in button

--- src/components/floatingButton.tsx
+++ src/components/floatingButton.tsx
@@ -3,7 +3,7 @@ import "./css/floatingButton.css"
 const FloatingButton = () => {
   return (
     <div className='whatsapp_div'>
-      <a href="https://api.whatsapp.com/send?phone=3884432288"><img src='./whatsapp.png' className="whatsapp_icon"/></a>
+      <a href="https://api.whatsapp.com/send?phone=3884482990"><img src='./whatsapp.png' className="whatsapp_icon"/></a>
     </div>
   );
 };
