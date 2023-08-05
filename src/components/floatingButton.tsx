a908dc15930dcfad5d185489a56a2ef41192572c
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-08-05 10:44:17 -0300

    add facebook floating button

--- src/components/floatingButton.tsx
+++ src/components/floatingButton.tsx
@@ -1,9 +1,15 @@
-import "./css/floatingButton.css"
+import "./css/floatingButton.css";
 
 const FloatingButton = () => {
   return (
     <div className='whatsapp_div'>
-      <a href="https://api.whatsapp.com/send?phone=3884482990"><img src='./whatsapp.png' className="whatsapp_icon"/></a>
+      <a href='https://www.facebook.com/profile.php?id=100063770313079'>
+        <img src='/facebook.png' className='whatsapp_icon' />
+      </a>
+      <hr className="my-1 border-0"/>
+      <a href='https://api.whatsapp.com/send?phone=543884482990'>
+        <img src='/whatsapp.png' className='whatsapp_icon' />
+      </a>
     </div>
   );
 };
