f1248fc7e674f4e757566df1df138732396cdef2
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-08-05 21:58:59 -0300

    add mobile category nav

--- src/components/mininav.tsx
+++ src/components/mininav.tsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ContentContext } from "../contexts/contentContext";
+import { Category } from "../types";
 
 type Props = {
-  product?: string
-}
+  product?: string;
+  hideMobileNav?: boolean;
+  categories?: Category[];
+};
 
-function Mininav({ product }: Props) {
+function Mininav({ product, hideMobileNav, categories }: Props) {
   const { filters, resetAll, activeCategory, setFilters } =
     useContext(ContentContext);
 
@@ -62,7 +65,7 @@ function Mininav({ product }: Props) {
         <li className='breadcrumb-item'>
           <Link
             to={`/category/${activeCategory}`}
-            className="text-white"
+            className='text-white'
             onClick={() => setFilters({ category: activeCategory as string })}
           >
             {activeCategory}
@@ -75,10 +78,47 @@ function Mininav({ product }: Props) {
     );
   };
 
+  function mobileList() {
+    if (!categories) return <></>;
+    let list = categories.map((category, index) => (
+      <li className='nav-item' key={index}>
+        <a href='#' className='nav-link ps-1 pe-0 py-2' onClick={() => setFilters({ category: category.name })}>
+          <span className={`badge rounded-pill bg-pink fs-6 fw-medium ${filters.category != category.name && "opacity-75"}`}>
+            {category.name}
+          </span>
+        </a>
+      </li>
+    ));
+    list.unshift(
+      <li className='nav-item' key={"todos"}>
+        <a href='#' className='nav-link ps-1 pe-0 py-2' onClick={() => setFilters({ category: "all" })}>
+          <span className={`badge rounded-pill bg-pink fs-6 fw-medium ${filters.category != "all" && "opacity-75"}`}>
+            Todos
+          </span>
+        </a>
+      </li>
+    );
+    return list
+  }
+
   return (
-    <nav aria-label='breadcrumb' className='row m-0 bg-light_blue'>
-      <ol className='breadcrumb mb-0 px-3 px-sm-5 py-2'>{tabsList()}</ol>
-    </nav>
+    <>
+      <nav
+        aria-label='breadcrumb'
+        className={`row m-0 bg-light_blue ${
+          hideMobileNav ? "" : "d-sm-block d-none"
+        }`}
+      >
+        <ol className='breadcrumb mb-0 px-3 px-sm-5 py-2'>{tabsList()}</ol>
+      </nav>
+      <nav
+        className={`row m-0 bg-white ${
+          hideMobileNav ? "d-none" : "d-flex d-sm-none"
+        }`}
+      >
+        <ul className='nav overflow-x-auto flex-nowrap ps-2'>{mobileList()}</ul>
+      </nav>
+    </>
   );
 }
 
