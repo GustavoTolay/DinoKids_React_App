3a33bd6e43a72040e5aa70f6a037b1a70dc5981a
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-08-07 21:44:00 -0300

    add purgecss to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import purgeCSS from "@fullhuman/postcss-purgecss"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        purgeCSS({
+          content: ["./index.html", "./src/main.tsx", "./src/App.tsx", "./src/**/*.tsx"]
+        })
+      ]
+    }
+  }
 })
