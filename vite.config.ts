35a2b92b96dc7b1ac7fb28fc693df8cf7c0e7146
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-10-15 09:45:15 -0300

    feat(dependencies): implement purge css dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import purgecss from "@fullhuman/postcss-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        purgecss({
+          content: [
+            "./index.html",
+            "./src/main.tsx",
+            "./src/App.tsx",
+            "./src/**/*.tsx",
+          ],
+        }),
+      ],
+    },
+  },
+});
