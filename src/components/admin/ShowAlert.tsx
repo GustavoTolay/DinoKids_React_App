3b10e57f1c1cb514077aad5894e61778f52bc156
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-10-18 18:26:41 -0300

    feat(components): add ShowAlert component

---
+++ src/components/admin/ShowAlert.tsx
@@ -0,0 +1,21 @@
+import { UseAlert } from "../../hooks/useAlert";
+
+type Props = {
+  alert: UseAlert;
+};
+
+function ShowAlert({ alert }: Props) {
+  return (
+    <div hidden={alert.state == undefined}>
+      <div
+        className={`alert alert-${alert.state?.color} alert-dismissible col-3 mx-auto`}
+        role='alert'
+      >
+        {alert.state?.message}
+        <button type='button' className='btn-close' onClick={alert.close} />
+      </div>
+    </div>
+  );
+}
+
+export default ShowAlert;
