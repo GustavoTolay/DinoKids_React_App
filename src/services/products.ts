70cd2a6bdfbdba60488fbbf10aef12c4c80d8304
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-10-20 17:05:34 -0300

    feat(services): add deleteProduct function

--- src/services/products.ts
+++ src/services/products.ts
@@ -4,17 +4,39 @@ export async function postProduct(
   product: NewProduct,
   image: File,
   token: string
-): Promise<Response> {
-  const formData = new FormData();
-  formData.append("image", image as Blob);
-  formData.append("product", JSON.stringify(product));
-  const options: RequestInit = {
-    method: "POST",
-    body: formData,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  const response = await fetch("https://dinokids.site/products", options);
-  return response;
+) {
+  try {
+    const formData = new FormData();
+    formData.append("image", image as Blob);
+    formData.append("product", JSON.stringify(product));
+    const options: RequestInit = {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch("https://dinokids.site/products", options);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteProduct(productId: string, token: string) {
+  try {
+    const options: RequestInit = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(
+      "https://dinokids.site/products/" + productId,
+      options
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
