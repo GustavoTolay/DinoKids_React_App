93a03e03c07096dfb6bd46829b362450c887b750
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-11-19 19:47:32 -0300

    feat(pages): add alert to Creator page

--- src/pages/Creator.tsx
+++ src/pages/Creator.tsx
@@ -1,17 +1,29 @@
-import Navbar from "../components/navbar"
-// import Itemform from "../components/Itemform"
-import NewItemForm from "../components/newItemForm"
+import Navbar from "../components/navbar";
+import ItemForm from "../components/admin/ItemForm";
+import useAlert from "../hooks/useAlert";
+import ShowAlert from "../components/admin/ShowAlert";
+import { Product } from "../types";
+import { useLocation } from "react-router-dom";
 
 const Creator = () => {
-  return <>
-    <div className='App container-fluid p-0 m-0'>
-        <Navbar categories={null}/>
-        <div className="row mx-0 mt-3"><h4 className="fw-semibold">Agregar Producto</h4></div>
+  const location = useLocation();
+  const { product }: { product?: Product } = location.state;
+  const alert = useAlert();
+
+  return (
+    <>
+      <div className='App container-fluid p-0 m-0'>
+        <Navbar categories={null} />
+        <div className='row mx-0 mt-3'>
+          <h4 className='fw-semibold'>Agregar Producto</h4>
+        </div>
         <div className='row m-0'>
-          <NewItemForm />
+          <ItemForm alert={alert} product={product} />
+          <ShowAlert alert={alert}></ShowAlert>
         </div>
       </div>
-  </>
-}
+    </>
+  );
+};
 
-export default Creator
+export default Creator;
